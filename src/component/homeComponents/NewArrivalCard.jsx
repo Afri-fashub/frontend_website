7af517e945301d430a3954560fb14e639fb0212d
InternetOG <omoleyeosadare@gmail.com>
2025-01-12 19:45:34 +0100

    image fix

--- src/component/homeComponents/NewArrivalCard.jsx
+++ src/component/homeComponents/NewArrivalCard.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { CiStar } from "react-icons/ci";
+import image from '../../assets/images/image.png'
 
 const NewArrivalCard = ({product}) => {
   return (
         <section className='grid gap-4'>
             <div>
-                <img src={product.img} width={'100%'} alt="" />
+                <img src={image} width={'100%'} alt="" />
                 <p className='font-bold text-base mt-4'>{product.title}</p>
             </div>
             <div className='flex justify-between'>
