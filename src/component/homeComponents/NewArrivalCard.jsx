ebd512f2fc14b945bf35879afbb11d8c6f87a213
InternetOG <omoleyeosadare@gmail.com>
2025-01-12 19:47:51 +0100

    image fix

--- src/component/homeComponents/NewArrivalCard.jsx
+++ src/component/homeComponents/NewArrivalCard.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
+import React from "react";
 import { CiStar } from "react-icons/ci";
-import image from '../../assets/images/image.png'
+import image from "../../assets/images/image.png";
 
-const NewArrivalCard = ({product}) => {
+const NewArrivalCard = ({ product }) => {
   return (
-        <section className='grid gap-4'>
-            <div>
-                <img src={image} width={'100%'} alt="" />
-                <p className='font-bold text-base mt-4'>{product.title}</p>
-            </div>
-            <div className='flex justify-between'>
-                <p className='font-bold text-base'>{product.price}</p>
-                <ul className='flex'>
-                    <li><CiStar className='text-xl' /></li>
-                    <li><CiStar className='text-xl' /></li>
-                    <li><CiStar className='text-xl' /></li>
-                    <li><CiStar className='text-xl' /></li>
-                    <li><CiStar className='text-xl' /></li>
-                </ul>
-            </div>
-        </section>
-  )
-}
+    <section className="grid gap-4">
+      <div>
+        <img src={image} width={"100%"} alt="" />
+        <p className="font-bold text-base mt-4">{product.title}</p>
+      </div>
+      <div className="flex justify-between">
+        <p className="font-bold text-base">{product.price}</p>
+        <ul className="flex">
+          <li>
+            <CiStar className="text-xl" />
+          </li>
+          <li>
+            <CiStar className="text-xl" />
+          </li>
+          <li>
+            <CiStar className="text-xl" />
+          </li>
+          <li>
+            <CiStar className="text-xl" />
+          </li>
+          <li>
+            <CiStar className="text-xl" />
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
 
-export default NewArrivalCard
+export default NewArrivalCard;
